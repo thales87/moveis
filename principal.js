5cd7c61d90df5959b94d1edb94147c19206bfbcf
Thales Ramalho de Souza <thales87@gmail.com>
2021-06-18 10:00:10 -0300

    Diminuindo o intervalo do banner

--- principal.js
+++ principal.js
@@ -2,6 +2,6 @@ var banners = ["Os melhores do Brasil!", "Qualidade e preço baixo!"];
 var bannerAtual = 0;
 function trocaBanner() {
 	bannerAtual = (bannerAtual + 1) % 2;
-	document.querySelector(’h2#mensagem’).textContent = banners[bannerAtual];
+	document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
 }
-setInterval(trocaBanner, 2000);
+setInterval(trocaBanner, 1000);
